f4a617838d377b1b28ef467dc6baa058cbc69027
laonayt <laonayt@163.com>
2020-09-09 00:12:21 +0800

    a test

---
+++ list.js
@@ -0,0 +1,25 @@
+var xuwei = [{
+    "artist": "专辑-像风一样自由 - 许巍.mp3",
+    "url": "https://cdn.jsdelivr.net/gh/laonayt/xuwei-resource@master/默认/像风一样自由 - 许巍.mp3"
+}, {
+    "artist": "专辑-南无观世音菩萨 - 许巍.mp3",
+    "url": "https://cdn.jsdelivr.net/gh/laonayt/xuwei-resource@master/默认/南无观世音菩萨 - 许巍.mp3"
+}, {
+    "artist": "专辑-旅行 - 许巍.mp3",
+    "url": "https://cdn.jsdelivr.net/gh/laonayt/xuwei-resource@master/默认/旅行 - 许巍.mp3"
+}, {
+    "artist": "专辑-时光 - 许巍.mp3",
+    "url": "https://cdn.jsdelivr.net/gh/laonayt/xuwei-resource@master/默认/时光 - 许巍.mp3"
+}, {
+    "artist": "专辑-星空 - 许巍.mp3",
+    "url": "https://cdn.jsdelivr.net/gh/laonayt/xuwei-resource@master/默认/星空 - 许巍.mp3"
+}, {
+    "artist": "专辑-漫步 - 许巍.mp3",
+    "url": "https://cdn.jsdelivr.net/gh/laonayt/xuwei-resource@master/默认/漫步 - 许巍.mp3"
+}, {
+    "artist": "专辑-礼物 - 许巍.mp3",
+    "url": "https://cdn.jsdelivr.net/gh/laonayt/xuwei-resource@master/默认/礼物 - 许巍.mp3"
+}, {
+    "artist": "专辑-那一年 - 许巍.mp3",
+    "url": "https://cdn.jsdelivr.net/gh/laonayt/xuwei-resource@master/默认/那一年 - 许巍.mp3"
+}]
